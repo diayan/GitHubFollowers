d1fdf6b669b4574eb2473a35716398278a8f61fe
diayan <diayans@outlook.com>
2020-12-15 19:43:45 +0000

    refactor network call to use swift 5 result

--- GithubFellows/Screens/FollowerListViewController.swift
+++ GithubFellows/Screens/FollowerListViewController.swift
@@ -9,23 +9,24 @@
 import UIKit
 
 class FollowerListViewController: UIViewController {
-
+    
     var username: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result{
+            case .success(let followers):
+                print(followers.count)
+                print(followers)
+                
+            case .failure(let error ):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "Ok")
+                
             }
-            
-            print("followers.count: \(followers.count)")
-            print(followers)
         }
     }
     
