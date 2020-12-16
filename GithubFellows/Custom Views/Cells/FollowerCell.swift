37ebad53fbd6c9c1abeef70abb6568f2571d6436
diayan <diayans@outlook.com>
2020-12-16 16:21:50 +0000

    create custom imageview && follower cell for collection view

---
+++ GithubFellows/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,13 @@
+//
+//  FollowerCell.swift
+//  GithubFellows
+//
+//  Created by diayan siat on 16/12/2020.
+//  Copyright © 2020 Diayan Siat. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GithubFellows/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,21 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFellows
+//
+//  Created by diayan siat on 16/12/2020.
+//  Copyright © 2020 Diayan Siat. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
