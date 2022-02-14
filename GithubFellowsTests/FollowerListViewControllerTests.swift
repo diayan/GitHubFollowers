ee4b688fe5e0e66ab7b296de7b64d38703b76646
diayan <diayans@outlook.com>
2022-02-14 19:11:21 +0000

    add test target

---
+++ GithubFellowsTests/FollowerListViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  FollowerListViewControllerTests.swift
+//  GithubFellowsTests
+//
+//  Created by diayan siat on 14/02/2022.
+//  Copyright © 2022 Diayan Siat. All rights reserved.
+//
+
+import XCTest
+
+class FollowerListViewControllerTests: XCTestCase {
+    
+    func test() {
+        XCTFail("Initial failing test ")
+    }
+}
